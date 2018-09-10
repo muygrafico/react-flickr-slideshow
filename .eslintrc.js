ecd0f016abee297ba2963d1df40b56a19cfe8616
German Hernandez <muygrafico@gmail.com>
2018-09-10 12:18:18 -0500

    add component

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,16 @@ module.exports = {
     "rules": {
         "indent" : ["error", 4, { "ignoredNodes": ["JSXElement *"] }],
         "react/jsx-indent" : ["error", 2]
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+         "sourceType": "module",
+         "module": "commonjs"
+    },
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
     }
 };
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
 import logo from './logo.svg'
 import './App.css'
+import SearchBox from './components/SearchBox.js'
 
 class App extends Component {
     render () {
@@ -10,8 +11,8 @@ class App extends Component {
               <img src={logo}
                 className='App-logo' alt='logo'
               />
-
-                <h1 className='App-title'>Welcome to React</h1>
+                <SearchBox />
+                  <h1 className='App-title'>Welcome to React</h1>
             </header>
               <p className='App-intro'>
                 To get started, edit <code>src/App.js</code> and save to reload.

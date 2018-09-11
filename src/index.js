8bc68ce3724900bbbf44e1ccd9dfb12b4181be48
German Hernandez <muygrafico@gmail.com>
2018-09-11 12:28:37 -0500

    ass sass/scss support | add basic scss file structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store'
-import './index.css'
+import './sass/main.scss'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
